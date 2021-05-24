fd1321af76a296bc2391fef76a08c7376c8c1d70
HimaniD22 <himanidautpure@gmail.com>
2021-05-24 23:32:50 +0800

    added model for slider and fetching data for slider from backend

--- tubers/tubers/settings.py
+++ tubers/tubers/settings.py
@@ -128,6 +128,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'tubers/static')
 ]
 
+# #Set media path
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIAFILES_DIRS = [
+#     os.path.join(BASE_DIR,'tubers/media')
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
